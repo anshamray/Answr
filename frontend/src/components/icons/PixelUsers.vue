f67cc019ab8a1c27c8bd6c6a245f859373d2b084
Anastasia Shamray <shamrayberlin@gmail.com>
2026-02-25 10:09:19 +0100

    fix: update PixelUsers component layout and remove unnecessary translation adjustment in LandingPage
    - Adjusted the layout of the PixelUsers component by modifying the positioning of SVG elements for better visual representation.
    - Removed the translate class from the PixelUsers component in LandingPage for improved alignment.

--- frontend/src/components/icons/PixelUsers.vue
+++ frontend/src/components/icons/PixelUsers.vue
@@ -6,15 +6,15 @@ defineProps({
 
 <template>
   <svg :width="size" :height="size" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-    <rect x="4" y="4" width="3" height="3" fill="currentColor"/>
-    <rect x="4" y="8" width="3" height="3" fill="currentColor"/>
-    <rect x="1" y="11" width="3" height="3" fill="currentColor"/>
+    <rect x="4" y="7" width="3" height="3" fill="currentColor"/>
     <rect x="4" y="11" width="3" height="3" fill="currentColor"/>
-    <rect x="7" y="11" width="3" height="3" fill="currentColor"/>
-    <rect x="14" y="4" width="3" height="3" fill="currentColor"/>
-    <rect x="14" y="8" width="3" height="3" fill="currentColor"/>
-    <rect x="11" y="11" width="3" height="3" fill="currentColor"/>
+    <rect x="1" y="14" width="3" height="3" fill="currentColor"/>
+    <rect x="4" y="14" width="3" height="3" fill="currentColor"/>
+    <rect x="7" y="14" width="3" height="3" fill="currentColor"/>
+    <rect x="14" y="7" width="3" height="3" fill="currentColor"/>
     <rect x="14" y="11" width="3" height="3" fill="currentColor"/>
-    <rect x="17" y="11" width="3" height="3" fill="currentColor"/>
+    <rect x="11" y="14" width="3" height="3" fill="currentColor"/>
+    <rect x="14" y="14" width="3" height="3" fill="currentColor"/>
+    <rect x="17" y="14" width="3" height="3" fill="currentColor"/>
   </svg>
 </template>
